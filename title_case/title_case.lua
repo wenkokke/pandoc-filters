db3339dac8bdbb6dc8052e653150ddb3a505f8b6
Wen Kokke <wenkokke@users.noreply.github.com>
2024-06-20 16:47:34 +0100

    Update title case

--- title_case/title_case.lua
+++ title_case/title_case.lua
@@ -4,28 +4,35 @@
 ---@author Wen Kokke
 ---@license MIT
 ---@copyright Wen Kokke 2024
+--------------------------------------------------------------------------------
+--
 -- Possible values for 'title_case' option:
 local TITLE_CASE_TITLE = 'title'
 local TITLE_CASE_SENTENCE = 'sentence'
-
+--
 -- Possible values for 'title_case_style' option:
 local TITLE_CASE_STYLE = 'AP'
-
+--
 -- Exceptions for each title case style:
 local TITLE_CASE_EXCEPTIONS = {
     AP = {'a', 'for', 'so', 'an', 'in', 'the', 'and', 'nor', 'to', 'at', 'of', 'up', 'but', 'on', 'yet', 'by', 'or'},
-    CONTRACTIONS = {'d', 'll', 's'}
+    -- Exceptions regardless of style
+    ['*'] = {'d', 'll', 's', 't'}
 }
-
+--
 -- Default options:
 local title_case = {
     title_case = TITLE_CASE_TITLE,
     title_case_style = TITLE_CASE_STYLE
 }
 
-local function title_case_exceptions()
+--------------------------------------------------------------------------------
+-- Filter that converts headers to title case.
+--------------------------------------------------------------------------------
+
+local function get_exceptions()
     if title_case.title_case_exceptions == nil then
-        local result = pandoc.List(TITLE_CASE_EXCEPTIONS.CONTRACTIONS)
+        local result = pandoc.List(TITLE_CASE_EXCEPTIONS['*'])
         if title_case.title_case == TITLE_CASE_TITLE then
             local title_case_exceptions = TITLE_CASE_EXCEPTIONS[title_case.title_case_style]
             if title_case_exceptions ~= nil then
@@ -37,8 +44,8 @@ local function title_case_exceptions()
     return title_case.title_case_exceptions
 end
 
-local function title_case_exception(word)
-    return title_case_exceptions():includes(word)
+local function is_exception(word)
+    return get_exceptions():includes(word)
 
 end
 
@@ -46,7 +53,7 @@ local PATTERN_UNICODE_CHARACTER = "([%z\1-\127\194-\244][\128-\191]*)"
 local PATTERN_WORD_CHARACTER = "[abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ]"
 
 local function convert_word(word)
-    if not title_case_exception(word) then
+    if not is_exception(word) then
         if word:len() >= 2 then
             word = word:sub(1, 1):upper() .. word:sub(2):lower()
         else
@@ -57,38 +64,80 @@ local function convert_word(word)
     return word
 end
 
-local function convert_Str(str)
-    local text = ""
-    local word = ""
-    for char in str.text:gmatch(PATTERN_UNICODE_CHARACTER) do
-        local is_word_char = char:match(PATTERN_WORD_CHARACTER) ~= nil
-        if is_word_char then
-            word = word .. char
-        else
-            if word ~= "" then
-                text = text .. convert_word(word)
-                word = ""
+local convert_Str = {
+    Str = function(str)
+        local text = ""
+        local word = ""
+        for char in str.text:gmatch(PATTERN_UNICODE_CHARACTER) do
+            local is_word_char = char:match(PATTERN_WORD_CHARACTER) ~= nil
+            if is_word_char then
+                word = word .. char
+            else
+                if word ~= "" then
+                    text = text .. convert_word(word)
+                    word = ""
+                end
+                text = text .. char
             end
-            text = text .. char
+        end
+        if word ~= "" then
+            text = text .. convert_word(word)
+            word = ""
+        end
+        return pandoc.Str(text)
+    end
+}
+
+local convert_Header = {
+    Header = function(header)
+        header = pandoc.walk_block(header, convert_Str)
+        return header
+    end
+}
+--------------------------------------------------------------------------------
+-- Filter that reads the options from the metadata.
+--------------------------------------------------------------------------------
+
+--- Extend a table with the values from another table.
+---
+--- This function mutates its first argument.
+---
+---@param t1 table
+---@param t2 table
+---@return table
+local function table_merge(t1, t2)
+    assert(type(t1) == 'table')
+    assert(type(t2) == 'table')
+    for key, val in pairs(t2) do
+        if type(val) == 'table' then
+            if type(t1[key] or nil) == 'table' then
+                t1[key] = table_merge(t1[key], val)
+            else
+                t1[key] = val
+            end
+        else
+            t1[key] = val
         end
     end
-    if word ~= "" then
-        text = text .. convert_word(word)
-        word = ""
-    end
-    return pandoc.Str(text)
+    return t1
 end
 
-local function convert_Header(header)
-    header = pandoc.walk_block(header, {
-        Str = convert_Str
-    })
-    return header
-end
+local get_options = {
+    Meta = function(el)
+        for key, value in pairs(el) do
+            if key == 'title_case' then
+                table_merge(title_case, value)
+            end
+        end
+    end
+}
+
+--------------------------------------------------------------------------------
+-- Filter that puts everything together.
+--------------------------------------------------------------------------------
 
 function Pandoc(doc)
-    doc = doc:walk({
-        Header = convert_Header
-    })
+    doc:walk(get_options)
+    doc = doc:walk(convert_Header)
     return doc
 end
